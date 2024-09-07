a2ec1ef3e73f898af2e5765f046224bd450c9b82
ArthurRudson <arthurrudson@gmail.com>
2024-09-07 12:13:31 -0300

    Moving pieces

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,11 +1,11 @@
 package chess;
 
-public class ChessException extends RuntimeException {
+import boardgame.BoardExeptin;
+
+public class ChessException extends BoardExeptin {
 	private static final long serialVersionUID = 1L;
 
 	public ChessException(String msg) {
 		super(msg);
 	}
-	
-	
 }
